ea4178b260cfa66e3a06c0409ae6f89df41c59c4
d198129 <70810114+d198129@users.noreply.github.com>
2020-10-12 17:29:56 +0800

    增加了判断是否碰撞的功能

--- js/flyBird/js/baseObj.js
+++ js/flyBird/js/baseObj.js
@@ -1,5 +1,22 @@
 var baseObj = {
     randomNum: function(min, max) {
-        return paseInt(Math.random() * (max - min + 1) + min);
+        return parseInt(Math.random() * (max - min + 1) + min);
+    },
+    //判断两个矩形是否发生碰撞
+    rectangleCrashEXamine: function(obj1, obj2) {
+        var obj1Left = obj1.offsetLeft;
+        var obj1Width = obj1.offsetLeft + obj1.offsetWidth;
+        var obj1Top = obj1.offsetTop
+        var obj1Height = obj1.offsetTop + obj1.offsetWidth;
+
+        var obj2Left = obj2.offsetLeft;
+        var obj2Width = obj2.offsetLeft + obj2.offsetWidth;
+        var obj2Top = obj2.offsetTop
+        var obj2Height = obj2.offsetTop + obj2.offsetWidth;
+
+        if (!(obj1Left > obj2Width || obj1Width < obj2Left || obj1Top > obj2Height || obj1Height < obj2Top)) {
+            return true;
+        }
+        return false;
     }
 }
