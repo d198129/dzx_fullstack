49d39e0cf65f773f50a5d998d2f3e9ebefb6d663
d198129 <1612936195@qq.com>
2020-10-28 17:13:31 +0800

    继续微信小程序的制作———百度地图的定位

--- weixinAPP/douban-movie/pages/home/home.js
+++ weixinAPP/douban-movie/pages/home/home.js
@@ -1,66 +1,93 @@
 // pages/home/home.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.getCity(
+            // () => {
+            // }
+        )
+    },
+    getCity(succeed) {
+        //先拿到城市名称
+        //先拿到当前所在地的经纬度
+        wx.getLocation({
+            success: (res) => {
+                // console.log(res);
+                //将经纬度转换成具体的城市名
+                wx.request({
+                    url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+                    data: {
+                        ak: 'GzzKkOKXhAKgxZuleBZ7X8C8XbmeKt9I',
+                        output: 'json',
+                        coordtype: 'wgs84',
+                        location: `${res.latitude},${res.longitude}`
+                    },
+                    success: (result) => {
+                        console.log(result);
+                        //拿豆瓣的地址做接口请求
+                        //将获取到的城市名传给豆瓣api
+                        //拿到当前城市热映电影数据
+                    }
+                })
+            }
+        })
+    },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
 
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+    }
 })
