2a71f47be86d03ce1abd42774c51f48e9960dfba
d198129 <1612936195@qq.com>
2020-11-27 20:31:17 +0800

    用element做登录和详情界面

--- vue/element-ui/src/router/index.js
+++ vue/element-ui/src/router/index.js
@@ -1,12 +1,32 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "@/views/Login";
+import Home from "@/views/Home";
+import userInfo from "@/views/UserInfo";
+import Form from "@/views/form";
 
 Vue.use(VueRouter);
 export default new VueRouter({
     routes: [{
-        path: "/",
-        name: "Login",
-        component: Login,
-    }, ],
+            path: "/",
+            name: "Login",
+            component: Login,
+        },
+        {
+            path: "/Home",
+            name: "home",
+            component: Home,
+            children: [{
+                    path: "UserInfo",
+                    name: "userinfo",
+                    component: userInfo,
+                },
+                {
+                    path: "form",
+                    name: "form",
+                    component: Form,
+                },
+            ],
+        },
+    ],
 });
---
+++ vue/element-ui/src/views/UserInfo.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="user-info">
+    这是用户的详细信息页面
+    {{ value }}--{{ totalPrice }}
+    <ul>
+      <li v-for="(item, index) in date" :key="index">
+        {{ item.title }}--{{ item.date }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 3,
+      newNum: 0,
+      price: 100,
+      news: [
+        {
+          title: '"空降"不到一周 韩俊任吉林代省长 曾是刘鹤"副手"',
+          date: "2020/11/21",
+        },
+        {
+          title: "上门女婿成村霸，敲诈勒索故意纵火还打伤司法人员",
+          date: "2020/11/24",
+        },
+        {
+          title: "X光显示肚子内有异物 港媒:黄之锋被关医院单人囚室",
+          date: "2020/11/28",
+        },
+        {
+          title: "特朗普突然出手，举行了史上最短一次记者会！",
+          date: "2020/11/29",
+        },
+      ],
+    };
+  },
+  created() {
+    this.newNum = this.num ** 3;
+  },
+  computed: {
+    value() {
+      return this.newNum ** 4;
+    },
+    totalPrice() {
+      return this.num ** 4;
+    },
+    date() {
+      let newss = this.news;
+      return newss.reverse();
+    },
+  },
+};
+</script>
+
+<style>
+</style>
