08252765b79321aa10fdc0ac157967d36abdfe51
d198129 <1612936195@qq.com>
2020-11-27 21:55:08 +0800

    写微信小程序的项目

--- weixinAPP/PYG/pages/index/index.js
+++ weixinAPP/PYG/pages/index/index.js
@@ -1,12 +1,58 @@
+//引入 用来发送请求的 方法
+import { request } from "../../request/index";
 //index.js
 //Page Object
 Page({
     data: {
-
+        //轮播图数组
+        swiperList: [],
+        //导航数组
+        catesList: [],
+        //楼层数据
+        floorList: []
     },
     //options(Object)
     onLoad: function(options) {
-
+        //发送异步请求获取轮播图 优化的手段可以通过es6的promise来解决这个问题
+        // wx.request({
+        //     url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+        //     success: (result) => {
+        //         console.log(result);
+        //         this.setData({
+        //             swiperList: result.data.message
+        //         })
+        //     },
+        // });
+        this.getSwiperList();
+        this.getCatesList();
+        this.getfloorList();
+    },
+    //获取轮播图
+    getSwiperList() {
+        request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata' })
+            .then(result => {
+                this.setData({
+                    swiperList: result.data.message
+                })
+            })
+    },
+    //获取 分类导航数据
+    getCatesList() {
+        request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems' })
+            .then(result => {
+                this.setData({
+                    catesList: result.data.message
+                })
+            })
+    },
+    //获取楼层数据
+    getfloorList() {
+        request({ url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata' })
+            .then(result => {
+                this.setData({
+                    floorList: result.data.message
+                })
+            })
     },
     onReady: function() {
 
