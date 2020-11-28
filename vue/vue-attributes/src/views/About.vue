3375c72cc513d4baf4d9f77929f054db8f09d1b7
d198129 <1612936195@qq.com>
2020-11-28 13:13:26 +0800

    学习了监听、混入、扩展

---
+++ vue/vue-attributes/src/views/About.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="about">
+    <p>当前温度：{{temperture.num}}</p>
+    <p>推荐穿衣：{{suggest}}</p>
+    <div>
+      <button @click="add">+</button>
+      <button @click="reduce">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0,
+      temperture: {
+        num: 9
+      },
+      suggest: ''
+    }
+  },
+  methods: {
+    add () {
+      this.temperture.num += 5
+    },
+    reduce () {
+      this.temperture.num -= 5
+    }
+  },
+  computed: {
+    numb () {
+      let a = this.count
+      a = a + 10
+      return a ** 2
+    }
+    // suggest1 () {
+    //   if (this.temperture <= -1) {
+    //     return '羽绒服'
+    //   } else if (this.temperture < 14) {
+    //     return '棉袄'
+    //   } else if (this.temperture < 24) {
+    //     return '夹克'
+    //   } else {
+    //     return '短袖'
+    //   }
+    // }
+  },
+  watch: {
+    // temperture (newVal, oldVal) {
+    //   if (newVal <= -1) {
+    //     this.suggest = '羽绒服'
+    //   } else if (newVal < 14) {
+    //     this.suggest = '棉袄'
+    //   } else if (newVal < 24) {
+    //     this.suggest = '夹克'
+    //   } else {
+    //     this.suggest = '短袖'
+    //   }
+    // }
+    temperture: {
+      handler (newVal, oldVal) {
+        if (newVal.num <= -1) {
+          this.suggest = '羽绒服'
+        } else if (newVal.num < 14) {
+          this.suggest = '棉袄'
+        } else if (newVal.num < 24) {
+          this.suggest = '夹克'
+        } else {
+          this.suggest = '短袖'
+        }
+      },
+      immediate: true,
+      deep: true
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue/vue-attributes/src/views/home.js
@@ -0,0 +1,11 @@
+const addLog = {
+  updated () {
+    console.log('数据发生变化')
+  }
+}
+const app = {
+  created () {
+    console.log('我是扩展的created')
+  }
+}
+module.exports = { addLog, app }
