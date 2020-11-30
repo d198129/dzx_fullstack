c0d18cf97cdf18797d6c7279b74b1016fa130bbd
d198129 <1612936195@qq.com>
2020-11-30 13:13:58 +0800

    写微信小程序

--- weixinAPP/PYG/pages/category/category.js
+++ weixinAPP/PYG/pages/category/category.js
@@ -9,22 +9,47 @@ Page({
         leftMenuList: [],
         //右侧的商品数据
         rightContent: [],
+        //被点击的左侧菜单
+        currentIndex:0
     },
     Cates: [],
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function(options) {
-        this.getCates();
+    onLoad: function (options) {
+        //判断一下本地存储有没有旧的数据
+        const Cates = wx.getStorage("cates");
+        if (!Cates) {
+            //不存在
+            this.getCates();
+        } else {
+            //有旧的数据
+            if (Date.now() - Cates.time > 1000 * 10) {
+                this.getCates();
+            } else {
+                //用旧数据
+                this.Cates = Cates.data;
+                let leftMenuList = this.Cates.map((v) => {
+                    return v.cat_name;
+                });
+                let rightContent = this.Cates[0].children;
+                this.setData({
+                    leftMenuList,
+                    rightContent,
+                });
+            }
+        }
     },
     //获取分类数据
     getCates() {
         request({
             url: "https://api-hmugo-web.itheima.net/api/public/v1/categories",
         }).then((res) => {
-            console.log(res);
+            // console.log(res);
             this.Cates = res.data.message;
+            //把接口的数据存入本地存储中
+            wx.setStorage("cates", {time:Date.now(),data:this.Cates});
             //构造左侧的大菜单数据
             let leftMenuList = this.Cates.map((v) => {
                 return v.cat_name;
@@ -35,6 +60,16 @@ Page({
                 rightContent,
             });
         });
+    },
+    //左侧菜单栏的点击事件
+    handleItemTap(e) {
+        console.log(e);
+        const { index } = e.currentTarget.dataset;
+        let rightContent = this.Cates[index].children;
+        this.setData({
+            currentIndex: index,
+            rightContent,
+        })
     },
     /**
      * 生命周期函数--监听页面初次渲染完成
