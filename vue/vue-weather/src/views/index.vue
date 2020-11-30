853c4a1e10912be28705163fbe1c6fa9e3a4b830
d198129 <1612936195@qq.com>
2020-11-30 16:33:37 +0800

    完善了部分代码

--- vue/vue-weather/src/views/index.vue
+++ vue/vue-weather/src/views/index.vue
@@ -14,9 +14,17 @@
       <div class="detail">
         <span>风力:{{ info.windPower }}</span> |
         <span>风向:{{ info.windDirection }}<span>风</span></span> |
-        <span>空气湿度:{{info.humidity}}%</span>
+        <span>空气湿度:{{ info.humidity }}%</span>
       </div>
     </div>
+    <div class="future">
+      <div class="group">
+        明天:
+        <span class="tm">白天</span>
+        <span class="tm">夜间</span>
+      </div>
+    </div>
+    <div class="echart-container" ref="echartContainer"></div>
   </div>
 </template>
 
@@ -33,10 +41,12 @@ export default {
       this.localTime = this.getLocalTime();
     }, 1000);
     this.initMap();
-    setInterval(()=>{
+    setInterval(() => {
       this.initMap();
-    },600000)
-
+    }, 600000);
+  },
+  mounted(){
+    this.initEchart();
   },
   methods: {
     getLocalTime() {
@@ -51,11 +61,10 @@ export default {
           if (status === "complete" && result.info === "OK") {
             // 查询成功，result即为当前所在城市信息
             // console.log(result);
-            self.getCurrentCityData(result.city)
-            .then((res)=>{
-              // console.log(res);
-              self.info = res;
-            })
+            self.getCurrentCityData(result.city).then((res) => {
+              // let a = [...res];
+              self.info = res[0];
+            });
           }
         });
       });
@@ -66,13 +75,46 @@ export default {
         AMap.plugin("AMap.Weather", function () {
           //创建天气查询实例
           var weather = new AMap.Weather();
+          var res = [];
           //执行实时天气信息查询
           weather.getLive(city, function (err, data) {
-            console.log(err, data);
-            resolve(data);
+            // console.log(err, data);
+            res.push(data);
+            // resolve(data);
+          });
+          weather.getForecast(city, function (err, data) {
+            // console.log(err, data);
+            res.push(data);
+            resolve(res)
           });
         });
       });
+    },
+    initEchart() {
+      let dom = this.$refs.echartContainer;
+      var myChart = echarts.init(dom);
+      let app = {},options=null;
+      options = {
+          title: {
+            text: "ECharts 入门示例",
+          },
+          tooltip: {},
+          legend: {
+            data: ["销量"],
+          },
+          xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+          },
+          yAxis: {},
+          series: [
+            {
+              name: "销量",
+              type: "bar",
+              data: [5, 20, 36, 10, 10, 20],
+            },
+          ],
+        };
+      myChart.setOption(options);
     },
   },
 };
@@ -90,6 +132,7 @@ export default {
     padding: 10px;
   }
   .city-info {
+    text-align: center;
     p {
       margin-bottom: 10px;
     }
@@ -100,6 +143,22 @@ export default {
         font-style: normal;
       }
     }
+  }
+  .future {
+    margin-top: 30px;
+    padding: 0 10px;
+    .group {
+      height: 44px;
+      line-height: 44px;
+      border-radius: 4px;
+      background-color: rgba(255, 255, 255, 0.3);
+      color: rgba(16, 16, 16, 1);
+      font-size: 16px;
+    }
+  }
+  .echart-container {
+    width: 100%;
+    height: 55vh;
   }
 }
 </style>
