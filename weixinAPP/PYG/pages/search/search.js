b25a5abfe0b2f5042e65a2253b9b3a98aa3646e9
d198129 <1612936195@qq.com>
2020-12-12 22:30:37 +0800

    实现了商品搜索功能

--- weixinAPP/PYG/pages/search/search.js
+++ weixinAPP/PYG/pages/search/search.js
@@ -1,13 +1,54 @@
 // pages/search/search.js
+import regeneratorRuntime from '../../lib/runtime/runtime';
+import { request } from "../../request/index";
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goods: [],
+    //取消的按钮；
+    isFocus:false
   },
-
+  TimeId: null,
+  handleInput(e) {
+    // console.log(e);
+    const { value } = e.detail;
+    if (!value.trim()) {
+      this.setData({
+        goods: [],
+        isFocus: false,
+        //输入框的值
+        inpValue:""
+      })
+      //值不合法
+      return;
+    }
+    this.setData({
+      isFocus: true
+    });
+    //发送请求获取参数
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.qsearch(value);
+    },1000);
+  },
+  async qsearch(query) {
+    const res = await request({ url: "/goods/qsearch", data: { query } });
+    console.log(res);
+    this.setData({
+      goods:res
+    })
+  },
+  //点击取消按钮
+  handleCancel() {
+    this.setData({
+      inpValue: "",
+      isFocus: false,
+      goods:[]
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
