095d189c3b5d43f41de2b6446d4c815297213a0a
d198129 <1612936195@qq.com>
2020-12-22 17:53:25 +0800

    学习了往npm库里传组件

---
+++ vue/vuerify-code/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="ap">
+    <VerifyCode></VerifyCode>
+  </div>
+</template>
+
+<script>
+import VerifyCode from './verify-code'
+export default {
+  components: {
+    VerifyCode
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue/vuerify-code/src/verify-code.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="vcWrap">
+    <input type="number" name="" id="" v-for="n in len" :key="'codeInput' + n" v-model.trim.number="code[n-1]" @keydown="onkeydown(n)" @keyup="onkeyup(n)" :ref="'codeInput' + n">
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      len: this.length || 4, //验证码的长度
+      code: new Array(this.len),
+      keyCode: [48,49,50,51,52,53,53,55,56,57,96,97,98,99,100,101,102,103,104,105,8,13]
+    }
+  },
+  props: ['length'],
+  methods: {
+    onkeydown(n){
+      // console.log(e);
+      // console.log(event);
+      if(!this.keyCode.includes(event.keyCode)){
+        event.returnValue = false;
+      }else if (event.keyCode === 8 && n > 1 && this.code[n-1] === ''){
+        //让前一个input聚焦
+        this.$refs['codeInput' + (n-1)][0].focus();
+        event.returnValue = false
+      }
+    },
+    onkeyup(n){
+      console.log(n);
+      if(event.keyCode >= 48 && event.keyCode <=57 || event.keyCode >= 96 && event.keyCode <= 105){
+        this.$set(this.code,n-1,event.key)
+        if(n < this.len) this.$refs['codeInput' + (n + 1)][0].focus();
+        console.log(this.code);
+        this.$emit('sendCodeInput',this.code.join(''))
+      }else if (event.keyCode === 8){
+        this.$emit('sendCodeInput',this.code.join(''))
+      }else if(event.keyCode === 13){
+        if(this.code.join('').length === this.len) this.$emit('goNext')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.vcWrap input {
+  width: 40px;
+  height: 40px;
+  line-height: 40;
+  -webkit-appearance: none;
+  background-color: #f7f9fa;
+  border: 1px solid #f7f9fa;
+  box-sizing: border-box;
+  display: inline-block;
+  font-size: inherit;
+  outline: 0;
+  padding: 0 14px;
+  transition: border-color 0.2 cubic-bezier(.645,.045,.355,1);
+  border-radius: 4px;
+  margin-right: 1em;
+}
+.vcWrap input:last-child {
+  margin-right: 0;
+}
+</style>
