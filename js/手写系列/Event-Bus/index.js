f716d6ff73d0e6c88fbafa0177ec5f78dfdaa297
d198129 <1612936195@qq.com>
2020-12-25 18:51:16 +0800

    分析发布订阅者模式

---
+++ js/手写系列/Event-Bus/index.js
@@ -0,0 +1,24 @@
+class EventEmitter {
+  constructor() {
+    //存属事件
+    this.events = this.events || new Map();
+  }
+  //监听事件
+  addListener(type,fn) {
+    if (!this.events.get(type)) {
+      this.events.set(type,fn)
+    }
+  }
+  //触发事件
+  emit(type) {
+    let handle = this.events.get(type);
+    handle.apply(this,[...arguments].slice(1))
+  }
+  
+}
+let emitter = new EventEmitter();
+emitter.addListener('age', age => {
+  console.log(age);
+})
+
+emitter.emit('age', 18)
