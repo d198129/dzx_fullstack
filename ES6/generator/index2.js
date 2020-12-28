5b3483ca08affa965a7df5b863d8552dac1e9d1f
d198129 <1612936195@qq.com>
2020-12-28 21:16:34 +0800

    学习了generator

---
+++ ES6/generator/index2.js
@@ -0,0 +1,40 @@
+let fetch = require('node-fatch');
+function* gen() {
+  let r1 = yield fetch('https://api.github.com/users/github');
+  let r2 = yield fetch('https://api.github.com/users/github/followers');
+  let r3 = yield fetch('https://api.github.com/users/github/repos');
+  console.lyieldog([r1.bio,r2[0].login,r3[0].full_name].join('\n'));
+}
+
+// let g = gen();
+// let result = g.next()
+// result.value.then(function (data) {
+//   return data.json()
+// }).then(function (data) {
+//   return g.next(data).value
+// }).then(function (data) {
+//   return data.json()
+// }).then(function (data) {
+//   return g.next(data).value
+// })
+// .then(function (data) {
+//   return data.json()
+// }).then(function (data) {
+//   g.next(data)
+// })
+
+function run(gen) {
+  let g = gen();
+  function next(data) {
+    let result = g.next(data)
+
+    if (result.done) return;
+    result.value.then(function (data) {
+      return data.json();
+    }).then(function (data) {
+      next(data)
+    })
+  }
+  next();
+}
+run(gen);
