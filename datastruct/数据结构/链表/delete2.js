6025a1602d0a093c8f03f85afe77332369b077f4
d198129 <1612936195@qq.com>
2021-01-18 12:13:17 +0800

    学习了链表的合并、删除

---
+++ datastruct/数据结构/链表/delete2.js
@@ -0,0 +1,36 @@
+// 给定一个排序链表，删除所有的含有重复数字的节点，只保留原始链表中没有重复出现的数字
+
+// 1 > 2 > 3 > 3 > 4 > 4 > 5  // 1 > 2 > 5
+// 1 > 1 > 1 > 2 > 3   // 2 > 3
+
+// dummy
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+const deleteDuplicates = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+  // dummy
+  let dummy = new ListNode();
+  // dummy 永远指向头节点
+  dummy.next = head;
+  let cur = dummy;
+  // cur 后面必须要有两个节点
+  while (cur.next && cur.next.next) {
+    if (cur.next.val == cur.next.next.val) {
+      // 记录重复的值
+      let val = cur.next.val;
+      while (cur.next && cur.next.val == val) {
+        // 删除
+        cur = cur.next;
+      }
+    } else {
+      cur = cur.next
+    }
+  }
+  return dummy.next;
+}
