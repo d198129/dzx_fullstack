23721447fd3431e54b95134c97c97d37e9ae6334
d198129 <1612936195@qq.com>
2021-02-25 16:34:25 +0800

    学习v8引擎垃圾回收机制

---
+++ 浏览器工作原理/v8工作原理/垃圾回收机制/index.js
@@ -0,0 +1,23 @@
+function foo() {
+  var a = 1;
+  var b = {
+    name: '酒神'
+  };
+  function showName() {
+    var c = 2;
+    var d = { name: '周老板'}
+  }
+  showName();
+}
+
+foo();
+
+
+person = {
+  name: '王二',
+  like: {
+    sweet: {
+      type: '巧克力'
+    }
+  }
+}
