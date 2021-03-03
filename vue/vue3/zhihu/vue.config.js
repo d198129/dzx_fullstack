b385f8bacd604984fef4a529e64b3fdefbb20e2f
d198129 <1612936195@qq.com>
2021-03-03 20:40:05 +0800

    配置跨域代理

--- vue/vue3/zhihu/vue.config.js
+++ vue/vue3/zhihu/vue.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  deServer: {
+  devServer: {
     host: 'localhost',
     port: 8080,
     https: false,
     open: false,
+    proxy: {
+      '/api': {
+        target: 'http://api.vikingship.xyz/api/',
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 }
