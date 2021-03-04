55a5db8e2beb6513749be9f89ab36942550d1119
d198129 <1612936195@qq.com>
2021-03-04 12:00:11 +0800

    我的页面和发布页面

--- vue/find/find-client/src/views/Me.vue
+++ vue/find/find-client/src/views/Me.vue
@@ -1,16 +1,30 @@
 <template>
   <div>
     <SHead :back="false" :me="false" :name="'我的'"></SHead>
-    <Unlogin v-if="user"></Unlogin>
+    <Unlogin v-if="!user"></Unlogin>
+    <div class="wrapp" v-else>
+      <div class="userwrapper">
+        <div class="avatar" @click="preview([userInfo.avatar])" :style="`background-image:url(${userInfo.avatar})`"></div>
+        <div class="user">
+          <div class="username">用户名: &nbsp;{{userInfo.username}}</div>
+          <div class="usertel">账号: &nbsp;{{userInfo.usertel}}</div>
+        </div>
+      </div>
+      <div class="unlogin" @click="myrelease">我的发布</div>
+      <div class="unlogin" @click="update">修改信息</div>
+      <div class="unlogin">关于我们</div>
+      <div class="unlogin" @click="quik">退出登录</div>
+    </div>
     <navBar></navBar>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import navBar from '@/components/NavBar'
 import SHead from '@/components/SimpleHeader'
 import Unlogin from '@/components/Unlogin'
+import { ImagePreview, Dialog  } from 'vant';
 export default {
   components: {
     navBar,
@@ -19,15 +33,86 @@ export default {
   },
   data(){
     return {
-      
+      userInfo: {}
+    }
+  },
+  methods: {
+    ...mapActions(['reduceAction']),
+    preview(item){
+      ImagePreview(item);
+    },
+    quik(){
+      Dialog.confirm({
+      title: '操作',
+      message: '确认退出登录吗',
+    }).then(() => {
+        // on confirm
+        sessionStorage.removeItem('userInfo');
+        this.reduceAction();
+      })
+      .catch(() => {
+        // on cancel
+      });
+    },
+    update(){
+      this.$router.push({ path: 'userinfo', query: { userinfo: this.userInfo }})
+    },
+    myrelease(){
+      this.$router.push({ path: 'myrelease', query: { usertel: this.userInfo.usertel }})
     }
   },
   computed: {
     ...mapState(['user'])
+  },
+  created(){
+    this.userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.wrapp {
+  margin: 43px auto 0;
+  .unlogin {
+    width: 260px;
+    height: 60px;
+    border-radius: 30px;
+    margin: 10px auto;
+    font-size: 26px;
+    background-color:skyblue;
+    line-height: 60px;
+    font-weight: 700;
+    text-align: center;
+  }
+}
+.userwrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 120px;
+  margin-top: 10px;
+  background-color: skyblue;
+  .avatar {
+    flex: 0 0 80px;
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    margin-left: 10px;
+    margin-right: 20px;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+  }
+  .user{
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    .username {
+      margin-bottom: 10px;
+    }
+  }
+}
 
 </style>
---
+++ vue/find/find-client/src/views/addNote.vue
@@ -0,0 +1,148 @@
+<template>
+  <div>
+    <SHead :back="true" :name="'发布'" :me="false"></SHead>
+    <div class="wrapper">
+      <van-cell-group>
+        <van-field v-model="title" label="标题" placeholder="请输入标题" />
+        <van-field type="textarea" v-model="value" label="内容" autosize placeholder="请输入描述" maxlength="50" show-word-limit />
+      </van-cell-group>
+      <van-field
+        readonly
+        clickable
+        name="datetimePicker"
+        :value="time"
+        label="时间选择"
+        placeholder="点击选择时间"
+        @click="showPicker = true"
+      />
+      <van-popup v-model="showPicker" position="bottom">
+        <van-datetime-picker
+          type="time"
+          @confirm="onConfirm"
+          @cancel="showPicker = false"
+        />
+      </van-popup>
+      <van-uploader v-model="fileList" multiple :max-count="6" :after-read="afterRead" />
+      <div class="fabu" @click="addnote">发布</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Toast } from 'vant';
+import SHead from '@/components/SimpleHeader'
+export default {
+  components: {
+    SHead
+  },
+  data() {
+    return {
+      fileList: [],
+      title: '',
+      value: '',
+      time: '',
+      showPicker: false
+    }
+  },
+  methods: {
+    afterRead(e){
+      console.log(e);
+    },
+    onConfirm(time) {
+      this.time = time;
+      console.log(time);
+      this.showPicker = false;
+    },
+    addnote(){
+      if (this.title.trim() == '') {
+        Toast.fail({
+          message:'标题不能为空',
+          forbidClick: true,
+          duration: 1000
+        });
+        return;
+      }
+      if (this.value.trim() == '') {
+        Toast.fail({
+          message:'内容不能为空',
+          forbidClick: true,
+          duration: 1000
+        });
+        return;
+      }
+      if (this.time.trim() == '') {
+        Toast.fail({
+          message:'请输入时间',
+          forbidClick: true,
+          duration: 1000
+        });
+        return;
+      }
+      if (this.fileList.length < 1) {
+        Toast.fail({
+          message:'请上传至少一张图片',
+          forbidClick: true,
+          duration: 1000
+        });
+        return;
+      }
+      let picture = [];
+      this.fileList.forEach(item => {
+        picture.push(item.content);
+      })
+      let usertel = this.$route.query.usertel;
+      let time = this.time;
+      let title = this.title;
+      let desc = this.value;
+      this.$http({
+        method: 'post',
+        url: this.$util.baseUrl + 'notes/addnote',
+        data: {
+          usertel: usertel,
+          time: time,
+          title: title,
+          desc: desc,
+          picture: picture
+        }
+      }).then(res => {
+        // console.log(res);
+        if(res.data.code = '8000'){
+          Toast.success({
+            message: res.data.mess,
+            forbidClick: true,
+            duration: 1500
+          });
+          this.$router.go(-1);
+        }else{
+          Toast.fail({
+            message: '发布失败',
+            forbidClick: true,
+            duration: 1500
+          })
+        }
+      }).catch(err => {
+        console.log(err);
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.van-uploader {
+  margin-top: 20px;
+}
+.wrapper {
+  margin-top: 44px;
+  .fabu {
+    width: 240px;
+    height: 60px;
+    border-radius: 30px;
+    background-color: skyblue;
+    text-align: center;
+    line-height: 60px;
+    font-size: 30px;
+    margin: 20px auto;
+  }
+}
+</style>
