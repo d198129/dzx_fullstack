d0770422d2f98773e25cd996f95f3ef9e973f388
d198129 <1612936195@qq.com>
2021-03-08 22:51:48 +0800

    用react开始一个项目

---
+++ react/jianshu/src/App.js
@@ -0,0 +1,11 @@
+import { Globalstyle } from './style'
+function App() {
+  return (
+    <div className='dell'>
+      hello World
+      <Globalstyle></Globalstyle>
+    </div>
+  );
+}
+
+export default App;
