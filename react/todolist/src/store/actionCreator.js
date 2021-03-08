2456d8813d160373d31ee17402ca4ab5c3152231
d198129 <1612936195@qq.com>
2021-03-08 15:30:22 +0800

    redux中间件

--- react/todolist/src/store/actionCreator.js
+++ react/todolist/src/store/actionCreator.js
@@ -13,3 +13,9 @@ export const getDeleteItem = (index) => ({
   type: DELETE_TODO_ITEM,
   index
 })
+
+export const getTodolost = () => {
+  return (dispatch) => { // 本来action必须是一个对象，因为用了thunk，现在他可以是函数
+    // 接口请求
+  }
+}
